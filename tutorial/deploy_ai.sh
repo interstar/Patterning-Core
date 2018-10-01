c025645dfa67631ed285498692b6d39c388ee27a
phil jones <interstar@gmail.com>
2018-10-01 02:52:15 -0300

    Delete deploy_ai.sh

--- tutorial/deploy_ai.sh
+++
@@ -1,5 +0,0 @@
-lein clean
-
-lein cljsbuild once prod
-
-rsync -avr --delete-after resources/public/ /media/phil/MyData/new_dev_tree/alchemy-islands/ai_bootdown_site/assets/patterning/tutorial_site/
