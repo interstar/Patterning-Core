0305ee6bdaf2d77089a5221954bf5bea7d52275a
Phil Jones <interstar@gmail.com>
2025-11-20 22:03:02 +0000

    updated to version 6.1 because of major param order change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.alchemyislands/patterning "0.6.0"
+(defproject com.alchemyislands/patterning "0.6.1"
   :description "Generating Patterns with Clojure"
   :url "http://alchemyislands.com/"
   :license {:name "Gnu Lesser Public License"
